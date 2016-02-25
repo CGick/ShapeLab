93762754d783e904ba96fd068bc3989a96800185
SK207_29 <SK207_29@K207_W829.carrollcc.edu>
2016-02-24 19:46:02 -0500

    Added Square class

---
+++ ShapeLab/src/cis232ShapeLab/Square.java
@@ -0,0 +1,14 @@
+package cis232ShapeLab;
+
+public class Square extends Rectangle implements Shape {
+
+	public Square() {
+		
+	}
+
+	public Square(double length) {
+		super(length, length);
+		
+	}
+
+}
