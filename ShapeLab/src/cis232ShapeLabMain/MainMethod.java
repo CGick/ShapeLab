0f59dd0bae450c28203e735c60a44ac4e4f49d95
SK207_29 <SK207_29@K207_W829.carrollcc.edu>
2016-02-24 19:55:33 -0500

    update

--- ShapeLab/src/cis232ShapeLabMain/MainMethod.java
+++ ShapeLab/src/cis232ShapeLabMain/MainMethod.java
@@ -1,8 +1,13 @@
 package cis232ShapeLabMain;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class MainMethod {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+
+		
 		
 
 	}
